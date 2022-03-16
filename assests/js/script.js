137e929dda6a8209404ccc1746bdbac291be639d
bobpruz <bobpruz@gmail.com>
2022-03-16 15:39:51 -0500

    aded for loop to populate forecast adjusted style for font size adjusted column size

--- assests/js/script.js
+++ assests/js/script.js
@@ -6,39 +6,61 @@ var lon = "";
 var exclude = "exclude=minutely,hourly,alerts";
 var units = "imperial";
 
+var forecast = function (city, state) {
+  fetch(geoUrl + "appid=" + apiKey + "&q=" + city + "," + state + ",us")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      fetch(
+        url +
+          "appid=" +
+          apiKey +
+          "&units=" +
+          units +
+          "&" +
+          exclude +
+          "&lon=" +
+          data[0].lon +
+          "&lat=" +
+          data[0].lat
+      )
+        .then(function (weather) {
+          return weather.json();
+        })
+        .then(function (data1) {
+          $("#temp-now").text(
+            "Temperature: " + data1.current.temp + " \u00B0F"
+          );
+          $("#wind-now").text(
+            "Wind Speed: " + data1.current.wind_speed + " MPH"
+          );
+          $("#humidity-now").text("Humidity: " + data1.current.humidity + " %");
+          $("#uv-now").text("UV Index: " + data1.current.uvi);
+          $("#wicon-now").attr(
+            "src",
+            "http://openweathermap.org/img/wn/" +
+              data1.current.weather[0].icon +
+              ".png"
+          );
+          var s = 1
 
+          for (var i = 0; i < data1.daily.length && i < 5; i++) {
+            $("#wicon-" + s).attr(
+              "src",
+              "http://openweathermap.org/img/wn/" +
+                data1.daily[i].weather[0].icon +
+                ".png"
+            );
+            $("#temp-" + s).text("Temp: " + data1.daily[i].temp.day + " \u00B0F");
+            $("#wind-" + s).text(
+              "Wind Speed: " + data1.daily[i].wind_speed + " MPH"
+            );
+            $("#humidity-" + s).text("Humidity: " + data1.daily[i].humidity + " %");
+            s++
+          }
+        });
+    });
+};
 
-var forecast = function(city, state) {
-fetch(geoUrl + "appid=" + apiKey + "&q=" + city + "," + state + ",us")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    fetch(
-      url +
-        "appid=" +
-        apiKey +
-        "&units=" +
-        units +
-        "&" +
-        exclude +
-        "&lon=" +
-        data[0].lon +
-        "&lat=" +
-        data[0].lat
-    )
-      .then(function (weather) {
-        return weather.json();
-      })
-      .then(function (data1) {
-        console.log(data1.current);
-        $("#temp-now").text("Temperature: " + data1.current.temp + " \u00B0F")
-        $("#wind-now").text("Wind Speed: " + data1.current.wind_speed + " MPH")
-        $("#humidity-now").text("Humidity: " + data1.current.humidity + " %")
-        $("#uv-now").text("UV Index: " + data1.current.uvi)
-        $("#wicon-now").attr("src",'http://openweathermap.org/img/wn/' + data1.current.weather[0].icon + '.png');
-      });
-  });
-}
-
-forecast("Victoria", "MN")
+forecast("Victoria", "MN");
